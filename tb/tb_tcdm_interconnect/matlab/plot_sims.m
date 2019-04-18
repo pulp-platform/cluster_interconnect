8107264707b5c3477825f45747496bde5f23a8d2
Michael Schaffner <schaffner@iis.ee.ethz.ch>
2019-04-18 18:45:28 +0200

    Update matlab plotting script

--- tb/tb_tcdm_interconnect/matlab/plot_sims.m
+++ tb/tb_tcdm_interconnect/matlab/plot_sims.m
@@ -21,7 +21,7 @@ plot_tests(stats, {'16x16','16x32','16x64'}, configs);
 export_fig 'sim-results/bfly2_parallelism_16x' -png -pdf
 
 %% clos networks
-plot_tests(stats, [], {'clos_2mn', 'clos_mn', 'clos_m2n','lic'});
+plot_tests(stats, [], {'clos_2mn', 'clos_m1n', 'clos_m2n','lic'});
 export_fig 'sim-results/clos' -png -pdf
 
 %% selection
@@ -30,12 +30,25 @@ export_fig 'sim-results/selection' -png -pdf
 
 
 %% plot only banking factor 2
-plot_tests(stats, {'8x16', '16x32', '32x64'}, {'bfly2','clos_2mn', 'clos_m1n', 'clos_m2n','lic'});
+plot_tests(stats, {'8x16', '16x32', '32x64'}, {'bfly2_n1','clos_2mn', 'clos_m1n', 'clos_m2n','lic'});
 
 export_fig 'sim-results/bf2' -png -pdf
 
-%% 32
-scatterplot_tests(stats, '16x', {'clos_m0p5n', 'clos_m1n', 'clos_m2n', 'bfly2_n1', 'bfly2_n2','bfly2_n4', 'lic'},'random uniform (p_{req}=1.00)');
+%% 8 masters
+scatterplot_tests(stats, '8x', {'clos_2mn', 'clos_m1n', 'clos_m2n', 'bfly2_n1', 'bfly2_n2','bfly2_n4', 'lic'},'random uniform (p_{req}=1.00)');
+export_fig 'sim-results/pareto_8x' -png -pdf
+
+%% 16 masters
+scatterplot_tests(stats, '16x', {'clos_2mn', 'clos_m1n', 'clos_m2n', 'bfly2_n1', 'bfly2_n2','bfly2_n4', 'lic'},'random uniform (p_{req}=1.00)');
+export_fig 'sim-results/pareto_16x' -png -pdf
+
+%% 64 masters
+scatterplot_tests(stats, '32x', {'clos_2mn', 'clos_m1n', 'clos_m2n', 'bfly2_n1', 'bfly2_n2','bfly2_n4', 'lic'},'random uniform (p_{req}=1.00)');
+export_fig 'sim-results/pareto_32x' -png -pdf
+
+%% 64 masters
+scatterplot_tests(stats, '64x', {'clos_2mn', 'clos_m1n', 'clos_m2n', 'bfly2_n1', 'bfly2_n2','bfly2_n4', 'lic'},'random uniform (p_{req}=1.00)');
+export_fig 'sim-results/pareto_64x' -png -pdf
 
 %% renaming of existing files (if needed)
 
